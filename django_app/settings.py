93279db9b6dd99760413b23446acd4111fd24040
manikantha.panigrahi <manikantha.panigrahi@iris.co.uk>
2024-10-20 09:42:19 +0530

    allowed host changed

--- django_app/settings.py
+++ django_app/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG",'False') == True # should be added if Debug is false
 
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
